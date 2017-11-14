66316ec34cfe90282da620645ae1bd96352c4130
guillemelmor <guillemelmor@gmail.com>
2017-11-14 15:50:20 +0100

    Add files via upload

--- js/script_b.js
+++ js/script_b.js
@@ -36,7 +36,34 @@ var SMARTPHONE = {
 // juego
 var game = (function () {
   document.addEventListener("touchstart", touchHandler);
+  document.addEventListener("touchend", touchHandler_aux);
   document.addEventListener("mouseup", touchHandler);
+
+  var boton_left = false;
+  var boton_right = false;
+  var boton_up = false;
+  var boton_down = false;
+  var boton_atack = false;
+
+
+  function touchHandler(e) {
+    // arriba
+    if((playerX <= 170)&&(playerX >= 110)&&(playerY <= 75)&&(playerY >= 20)){
+      boton_up = false;
+    }
+    // abajo
+    if((playerX <= 175)&&(playerX >= 114)&&(playerY <= 140)&&(playerY >= 85)){
+      boton_down = false;
+    }
+    // derecha 
+    if((playerX <= 230)&&(playerX >= 175)&&(playerY <= 107)&&(playerY >= 54)){
+      boton_right = false;
+    }
+    // izquierda
+    if((playerX <= 109)&&(playerX >= 55)&&(playerY <= 107)&&(playerY >= 54)){
+      boton_left = false;
+    }
+  }
 
   function touchHandler(e) {
     if(e.touches) {
@@ -48,37 +75,52 @@ var game = (function () {
     if((playerX <= 170)&&(playerX >= 110)&&(playerY <= 75)&&(playerY >= 20)){
       console.log("flecha arriba")
       console.log(player_1.posY)
-      if(player_1.posY > 199.1){
-        player_1.posY -= 0.05;
-        player_1.posX += 6;
-        player_1_carril.x -= 10;
-        player_1_carril.y -= 5;                  
-      }
+      boton_up = true;
+      while(boton_up == true){
+        if(player_1.posY > 199.1){
+          player_1.posY -= 0.05;
+          player_1.posX += 6;
+          player_1_carril.x -= 10;
+          player_1_carril.y -= 5;                  
+        }
+      }      
     }
     if((playerX <= 175)&&(playerX >= 114)&&(playerY <= 140)&&(playerY >= 85)){
       console.log("flecha abajo")
       console.log(player_1.posY)
-      if(player_1.posY < 474){
-        player_1.posY += 0.05;
-        player_1.posX -= 6;
-        player_1_carril.x += 10;
-        player_1_carril.y += 5;
-      }
+      boton_down = true;
+      while(boton_down == true){
+        if(player_1.posY < 474){
+          player_1.posY += 0.05;
+          player_1.posX -= 6;
+          player_1_carril.x += 10;
+          player_1_carril.y += 5;
+        }
+      }      
     }
     if((playerX <= 109)&&(playerX >= 55)&&(playerY <= 107)&&(playerY >= 54)){
       console.log("flecha izquierda")
-      if (player_1.posX > (-120*player_1.posY + 56850))
+      boton_left = true;
+      while(boton_left == true){
+        if (player_1.posX > (-120*player_1.posY + 56850))
         player_1.posX -= player_1.speed;   
+      }
     }
     if((playerX <= 230)&&(playerX >= 175)&&(playerY <= 107)&&(playerY >= 54)){
       console.log("flecha derecha")
-      if (player_1.posX < (-120*player_1.posY + 57215))
+      boton_right = true;
+      while(boton_right == true){
+        if (player_1.posX < (-120*player_1.posY + 57215))
         player_1.posX += player_1.speed;
+      }
     }
     if((playerX <= 520)&&(playerX >= 465)&&(playerY <= 110)&&(playerY >= 55)){
-      player_1_bullet_x = player_1_carril.x - 10;
-      player_1_bullet_y = player_1_carril.y - 10;
-      player_1.shoot_touch();
+      boton_atack = true;
+      while(boton_atack == true){
+        player_1_bullet_x = player_1_carril.x - 10;
+        player_1_bullet_y = player_1_carril.y - 10;
+        player_1.shoot_touch();
+      }
     }
   }
   /*
